0d54b09eaa310acbe9c2bedaef4ac175a6aa4383
kohei.iwamoto <kohei.iwamoto17164@gmail.com>
2025-12-17 20:49:36 +0900

    Initial commit

---
+++ test/redmin_on_aws_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RedminOnAwsCdk from '../lib/redmin_on_aws_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/redmin_on_aws_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RedminOnAwsCdk.RedminOnAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
